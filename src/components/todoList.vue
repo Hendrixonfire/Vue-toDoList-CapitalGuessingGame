15822bbd792de9991b2113567b4ae7df7b2eb182
Hendrixonfire <129618415+Hendrixonfire@users.noreply.github.com>
2023-12-01 01:20:00 +0300

    Add files via upload

---
+++ src/components/todoList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h2>Список задач</h2>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        {{ task.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  mounted() {
+    this.fetchTasks();
+  },
+  methods: {
+    async fetchTasks() {
+      try {
+        const response = await axios.get('/api/tasks');
+        this.tasks = response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Стили для компонента ToDoList.vue */
+
+</style>
