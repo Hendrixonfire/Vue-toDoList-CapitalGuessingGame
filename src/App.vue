2ebc7a4dbc9b882ea5247d53cb96337a5a077cff
Hendrixonfire <129618415+Hendrixonfire@users.noreply.github.com>
2023-12-01 01:19:09 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+   
+    <router-link to="/">Home page</router-link>
+    <router-link to="/capitals">Guess a capital Game:</router-link>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped>
+/* Стили для About.vue */
+
+a{
+  display:inline;
+  text-decoration:none;
+  margin:auto;
+  padding:5px;
+
+}
+
+</style>
